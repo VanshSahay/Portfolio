888b97cfd85a27dfb96174ba39b2838de745206a
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-19 02:20:44 +0530

    added image cropped and fit in the div

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className='flex flex-col h-screen'>
       <main className='grow'>
         <article className='grid grid-cols-[2fr_1fr] grid-rows-[2fr_1.5fr] h-full'>
-          <div id='image-section' className='h-full border-l border-white/[0.1]'></div>
+          <div id='image-section' className='h-full w-full border-l border-white/[0.1] bg-cover bg-center' style={{backgroundImage: "url(../../../bg-1.jpg)"}}></div>
           <div id='description-section' className='h-full border-l border-white/[0.1]'></div>
           <div id='title-section' className='h-full border-t border-white/[0.1]'></div>
           <div id='nav-section' className='h-full border-t border-l border-white/[0.1]'></div>
