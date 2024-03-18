81d14c8d8dba85c8d267852dbabee441e61758c1
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-19 01:25:01 +0530

    Added grids for various article elems

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header text check</div>
+    <div className='flex flex-col h-screen'>
+      <main className='grow'>
+        <article className='grid grid-cols-[2fr_1fr] grid-rows-[2fr_1.5fr] h-full'>
+          <div id='image-section' className='h-full border-l border-white/[0.1]'></div>
+          <div id='description-section' className='h-full border-l border-white/[0.1]'></div>
+          <div id='title-section' className='h-full border-t border-white/[0.1]'></div>
+          <div id='nav-section' className='h-full border-t border-l border-white/[0.1]'></div>
+        </article>
+        <article className='grid h-full'></article>
+        <article className='grid h-full'></article>
+        <article className='grid h-full'></article>
+      </main>          
+    </div>
   )
 }
 
