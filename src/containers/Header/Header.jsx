e75b9a6119c774e7b5a8749732bb84ad66ad3123
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-19 04:02:11 +0530

    Added header article

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -6,11 +6,28 @@ function Header() {
       <main className='grow'>
         <article className='grid grid-cols-[2fr_1fr] grid-rows-[2fr_1.5fr] h-full'>
           <div id='image-section' className='h-full w-full border-l border-white/[0.1] bg-cover bg-center' style={{backgroundImage: "url(../../../bg-1.jpg)"}}></div>
-          <div id='description-section' className='h-full border-l border-white/[0.1]'></div>
-          <div id='title-section' className='h-full border-t border-white/[0.1]'></div>
-          <div id='nav-section' className='h-full border-t border-l border-white/[0.1]'></div>
+          <div id='description-section' className='flex items-end justify-center h-full border-l border-white/[0.1]'>
+            <p className='px-20 py-16 text-lg tracking-wide leading-[160%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur consequatur reprehenderit reiciendis amet sapiente illum adipisci aspernatur itaque dolor dolorem quos doloremque eaq</p>
+          </div>
+          <div id='title-section' className='h-full border-t flex justify-between border-white/[0.1]'>
+            <h1 className='text-7xl font-black tracking-widest text-white py-24 px-20' style={{fontFamily: "Bebas Neue"}}>RECREATING<br/>FUTURES</h1>
+            <a href="#">
+              <img src="../../../plus.svg" className='text-white py-24 px-24' alt="" />
+            </a>
+          </div>
+          <div id='nav-section' className='grid grid-cols-2 h-full border-t border-l border-white/[0.1]'>
+            <div className='h-full w-full border-l border-white/[0.1] flex justify-center'>
+              <a href="#">
+                <img src="../../../left-arrow.svg" className='py-36' alt="" />
+              </a>
+            </div>
+            <div className='h-full w-full border-l border-white/[0.1] flex justify-center'>
+              <a href="#">
+                <img src="../../../right-arrow.svg" className='py-36' alt="" />
+              </a>
+            </div>
+          </div>
         </article>
-        <article className='grid h-full'></article>
         <article className='grid h-full'></article>
         <article className='grid h-full'></article>
       </main>          
