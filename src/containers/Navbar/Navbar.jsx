4a5ba4bf75ab9bfd62d6cf6f5bb6eaf00feabbef
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-18 22:46:56 +0530

    completed navbar except icons that will be added later

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function Navbar() {
   return (
     <nav  className='flex w-full border border-white/[0.1]'>
-      <div id='nav-logo-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] basis-1/3 box-border'>
+      <div id='nav-logo-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] basis-1/3 box-border justify-start'>
         <a href="#">
           <img src="../../../vite.svg" alt="..." />
         </a>
       </div>
-      <div id='nav-link-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] basis-1/3 box-border'>
+      <div id='nav-link-section' className='flex gap-24 py-12 px-8 border-l border-white/[0.1] basis-1/3 box-border items-center justify-center'>
         <a href="#">ABOUT</a>
         <a href="#">WORK</a>
       </div>
-      <div id='nav-social-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] grow box-border'>
+      <div id='nav-social-section' className='flex gap-24 py-12 px-8 border-l border-white/[0.1] grow box-border items-center justify-center'>
         <a href="#">
           <img src="../../../vite.svg" alt="..." />
         </a>
@@ -23,7 +23,7 @@ function Navbar() {
           <img src="../../../vite.svg" alt="..." />
         </a>
       </div>
-      <div id='nav-contact-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] grow box-border'>
+      <div id='nav-contact-section' className='flex gap-4 py-12 px-8 border-l border-white/[0.1] grow box-border items-center justify-center'>
         <a href="#">GET IN TOUCH</a>
       </div>
     </nav>
