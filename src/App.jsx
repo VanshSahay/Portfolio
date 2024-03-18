a8f7c54d7d0fb4bf6687536e24bd6a2d1082cf1c
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-18 21:30:07 +0530

    initial setup with bg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar, Header, Background } from './containers'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <Background/>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hii THERE</h1>
+    </div>
+  )
+}
+
+export default Navbar
